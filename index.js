c4c71bc48970b08fe90f949ca7d5688d17e73be0
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-06-21 14:58:41 -0300

    req 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const getAllTalkers = require('./talkers/getAllTalkers');
 
+const getAllTalkers = require('./talkers/getAllTalkers');
 
 const app = express();
 app.use(bodyParser.json());
